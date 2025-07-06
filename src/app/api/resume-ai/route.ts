7e4396c4637275ac800f2c1ecfa2741b6dd5b92c
Harmeet Singh <hskhanduja03@gmail.com>
2025-07-06 06:14:08 +0530

    add resume ai

--- src/app/api/resume-ai/route.ts
+++ src/app/api/resume-ai/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const resumeLink =
-    process.env.RESUME_LINK ||
+    process.env.RESUME_LINK_AI ||
     "https://drive.google.com/file/d/1iEV5MRAxVPCnq37HSsAzjTni9xT0waT6/view";
 
   return NextResponse.redirect(resumeLink, 302);
